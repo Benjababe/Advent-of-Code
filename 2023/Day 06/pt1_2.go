3d4662596ca54ba3660b6b33c75927797c3cee49
Benjababe <bengohzy@gmail.com>
2023-12-06 13:53:02 +0800

    2023 d6

---
+++ 2023/Day 06/pt1_2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/benjababe/advent-of-code/helper"
+)
+
+func getButtonCombinations(time string, dist string) int64 {
+	timeInt, _ := strconv.ParseFloat(time, 64)
+	distInt, _ := strconv.ParseFloat(dist, 64)
+
+	disc := timeInt*timeInt - 4*-distInt*-1
+	x1 := math.Floor((-timeInt + math.Sqrt(disc)) / -2)
+	x2 := math.Floor((-timeInt - math.Sqrt(disc)) / -2)
+
+	return int64(x2 - x1)
+}
+
+func solve(lines []string) {
+	scores := []int64{1, 0}
+
+	time, dist := lines[0], lines[1]
+	times := strings.Fields(strings.Split(time, ":")[1])
+	dists := strings.Fields(strings.Split(dist, ":")[1])
+	cTime := strings.ReplaceAll(strings.Split(time, ":")[1], " ", "")
+	cDist := strings.ReplaceAll(strings.Split(dist, ":")[1], " ", "")
+
+	for i, time := range times {
+		scores[0] *= getButtonCombinations(time, dists[i])
+	}
+	scores[1] = getButtonCombinations(cTime, cDist)
+
+	fmt.Printf("Silver: %d\nGold: %d\n", scores[0], scores[1])
+}
+
+func main() {
+	lines := []string{}
+	helper.GetLines(&lines, "input.txt")
+
+	start := helper.GetCurrentTime()
+	solve(lines)
+	end := helper.GetCurrentTime()
+	helper.GetTimeTaken(start, end)
+}
