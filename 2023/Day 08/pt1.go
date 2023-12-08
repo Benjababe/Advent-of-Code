9618439616c73f6baee0d22af0b071169ee6c37c
Benjababe <bengohzy@gmail.com>
2023-12-08 19:05:54 +0800

    2023 d8 cleaned

--- 2023/Day 08/pt1.go
+++ 2023/Day 08/pt1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 
 	"github.com/benjababe/advent-of-code/helper"
@@ -10,8 +11,28 @@ import (
 func solve(lines []string) int64 {
 	score := int64(0)
 
-	for _, line := range lines {
-		helper.Unused(line)
+	instructions := lines[0]
+	nodeMap := make(map[string](map[rune]string))
+	nodeRegex := regexp.MustCompile(`(\w+)\s=\s\((\w+),\s(\w+)\)`)
+
+	for _, line := range lines[1:] {
+		if len(line) == 0 {
+			continue
+		}
+
+		match := nodeRegex.FindStringSubmatch(line)
+		src, l, r := match[1], match[2], match[3]
+		nodeMap[src] = map[rune]string{'L': l, 'R': r}
+
+		helper.Unused(line, match)
+	}
+
+	curNode := "AAA"
+	for curNode != "ZZZ" {
+		for _, inst := range instructions {
+			curNode = nodeMap[curNode][inst]
+			score++
+		}
 	}
 
 	return score
--- 2023/Day 08/pt2.go
+++ 2023/Day 08/pt2.go
@@ -2,18 +2,78 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/benjababe/advent-of-code/helper"
 )
 
-func solve(lines []string) int64 {
-	score := int64(0)
+func gcd(a, b int64) int64 {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
 
-	for _, line := range lines {
-		helper.Unused(line)
+func lcm(nums []int64) int64 {
+	if len(nums) == 0 {
+		panic("No numbers to lcm")
+	} else if len(nums) == 1 {
+		return nums[0]
 	}
 
+	res := nums[0] * nums[1] / gcd(nums[0], nums[1])
+
+	for _, num := range nums[2:] {
+		res = lcm([]int64{res, num})
+	}
+
+	return res
+}
+
+func solve(lines []string) int64 {
+	score := int64(0)
+	steps := int64(0)
+
+	curNodes := []string{}
+	minSteps := []int64{}
+	instructions := lines[0]
+	nodeMap := make(map[string](map[rune]string))
+	nodeRegex := regexp.MustCompile(`(\w+)\s=\s\((\w+),\s(\w+)\)`)
+
+	for _, line := range lines[1:] {
+		if len(line) == 0 {
+			continue
+		}
+
+		match := nodeRegex.FindStringSubmatch(line)
+		src, l, r := match[1], match[2], match[3]
+		nodeMap[src] = map[rune]string{'L': l, 'R': r}
+
+		if strings.HasSuffix(src, "A") {
+			curNodes = append(curNodes, src)
+			minSteps = append(minSteps, int64(0))
+		}
+	}
+
+	for helper.SlicesAny[int64](minSteps, func(steps int64) bool { return steps == 0 }) {
+		for _, inst := range instructions {
+			steps++
+
+			for i, node := range curNodes {
+				curNodes[i] = nodeMap[node][inst]
+
+				if strings.HasSuffix(curNodes[i], "Z") && minSteps[i] == 0 {
+					minSteps[i] = steps
+				}
+			}
+		}
+	}
+
+	score = lcm(minSteps[0:2])
 	return score
 }
 
