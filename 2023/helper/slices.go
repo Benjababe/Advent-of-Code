4d81624a467b6e49844508fd94fd0361a0f87e22
Benjababe <bengohzy@gmail.com>
2023-12-09 13:27:58 +0800

    update slice helper

--- 2023/helper/slices.go
+++ 2023/helper/slices.go
@@ -1,5 +1,8 @@
 package helper
 
+import "strconv"
+
+// Use a predicate function and returns if all element in the slice matches it
 func SlicesAll[T any](slice []T, pred func(T) bool) bool {
 	for _, elem := range slice {
 		if !pred(elem) {
@@ -9,6 +12,7 @@ func SlicesAll[T any](slice []T, pred func(T) bool) bool {
 	return true
 }
 
+// Use a predicate function and returns if any element in the slice matches it
 func SlicesAny[T any](slice []T, pred func(T) bool) bool {
 	for _, elem := range slice {
 		if pred(elem) {
@@ -16,4 +20,17 @@ func SlicesAny[T any](slice []T, pred func(T) bool) bool {
 		}
 	}
 	return false
+}
+
+func SliceInt64ToStr(slice []string) ([]int64, error) {
+	int64Slice := make([]int64, len(slice))
+	for i, elem := range slice {
+		num, err := strconv.ParseInt(elem, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		int64Slice[i] = num
+	}
+	return int64Slice, nil
 }
