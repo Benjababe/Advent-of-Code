8c84cfe650a62ab63804e36ed25c2e7c83c90173
Benjababe <bengohzy@gmail.com>
2023-12-18 14:23:11 +0800

    2023 d18

---
+++ 2023/Day 18/pt1_2.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+
+	"github.com/benjababe/advent-of-code/helper"
+)
+
+type Instruction struct {
+	dir   string
+	steps int64
+}
+
+type Vertex struct {
+	x int64
+	y int64
+}
+
+func polygonArea(vertices []Vertex) int64 {
+	area := float64(0)
+	n := len(vertices)
+
+	for i := 0; i < n; i++ {
+		j := (i + 1) % n
+		area += float64(vertices[i].x * vertices[j].y)
+		area -= float64(vertices[j].x * vertices[i].y)
+	}
+
+	area = math.Abs(area) / 2
+	return int64(area)
+}
+
+func parseLine(line string, hex bool) Instruction {
+	instruction := Instruction{}
+	r := regexp.MustCompile(`(\w)\s(\d+)\s\(#([\d\w]{5})(\d)\)`)
+	matches := r.FindStringSubmatch(line)
+
+	dirMap := map[string]string{"0": "R", "1": "D", "2": "L", "3": "U"}
+
+	if hex {
+		instruction.steps, _ = strconv.ParseInt(matches[3], 16, 64)
+		instruction.dir = dirMap[matches[4]]
+	} else {
+		instruction.steps, _ = strconv.ParseInt(matches[2], 10, 64)
+		instruction.dir = matches[1]
+	}
+
+	return instruction
+}
+
+func dig(lines []string, hex bool) int64 {
+	x, y := int64(0), int64(0)
+	vertices := []Vertex{}
+	borderLen := int64(0)
+
+	for _, line := range lines {
+		instruction := parseLine(line, hex)
+
+		switch instruction.dir {
+		case "L":
+			x -= instruction.steps
+		case "R":
+			x += instruction.steps
+		case "U":
+			y -= instruction.steps
+		case "D":
+			y += instruction.steps
+		}
+
+		vertices = append(vertices, Vertex{x, y})
+		borderLen += instruction.steps
+	}
+
+	area := polygonArea(vertices)
+	area += borderLen/2 + 1
+
+	return area
+}
+
+func solve(lines []string) (int64, int64) {
+	p1, p2 := dig(lines, false), dig(lines, true)
+	return p1, p2
+}
+
+func main() {
+	lines := []string{}
+	helper.GetLines(&lines, "input.txt")
+
+	start := helper.GetCurrentTime()
+	p1, p2 := solve(lines)
+	fmt.Printf("Silver: %d\nGold: %d\n", p1, p2)
+	end := helper.GetCurrentTime()
+	helper.GetTimeTaken(start, end)
+}
