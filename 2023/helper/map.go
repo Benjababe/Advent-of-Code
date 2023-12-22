1b5282e07db4eda85d40557aaa4d3686673b9bc8
Benjababe <bengohzy@gmail.com>
2023-12-22 15:44:47 +0800

    update go helper

--- 2023/helper/map.go
+++ 2023/helper/map.go
@@ -6,3 +6,24 @@ func ReadMapDefault[K comparable, V interface{}](m map[K]V, key K, defaultValue 
 	}
 	return defaultValue
 }
+
+func SetUnion[K comparable](m1, m2 map[K]bool) map[K]bool {
+	unionMap := make(map[K]bool)
+	for k := range m1 {
+		unionMap[k] = true
+	}
+	for k := range m2 {
+		unionMap[k] = true
+	}
+	return unionMap
+}
+
+func SetIntersect[K comparable](m1, m2 map[K]bool) map[K]bool {
+	intersectSet := make(map[K]bool)
+	for k := range m1 {
+		if _, ok := m2[k]; ok {
+			intersectSet[k] = true
+		}
+	}
+	return intersectSet
+}
