c597c183996bdbb529d6c6e692e8bb3915aaff79
benjababe <bengohzy@gmail.com>
2024-12-03 15:37:05 +0800

    feat: update 2024 d3 sol

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -6,7 +6,7 @@ mod day_03;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let big_boy: bool = args.contains(&"bigboy".to_string()) || args.contains(&"bb".to_string());
+    let big_boy: bool = args.contains(&"--bigboy".to_string()) || args.contains(&"-bb".to_string());
 
     day_01::solve(big_boy);
     day_02::solve(big_boy);
