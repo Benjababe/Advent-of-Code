7b6dc2cc7a16aa572c5873b6bcd29e2ec7c7cf24
benjababe <bengohzy@gmail.com>
2025-12-04 17:29:26 +0800

    feat: include timer to python boilerplate

--- boilerplates/boilerplate.py
+++ boilerplates/boilerplate.py
@@ -1,5 +1,15 @@
 import platform
 import subprocess
+import time
+
+
+def format_time(sec: float) -> str:
+    if sec < 0.001:
+        return f"{sec * 1_000_000:.2f} microseconds"
+    elif sec < 1:
+        return f"{sec * 1000:.2f} milliseconds"
+    else:
+        return f"{sec:.2f} seconds"
 
 
 def get_lines(filename: str):
@@ -24,9 +34,12 @@ def get_score(lines: list[str]) -> int:
 
 if __name__ == "__main__":
     lines = get_lines("input.txt")
-    score = get_score(lines)
 
+    t1 = time.perf_counter()
+    score = get_score(lines)
+    t2 = time.perf_counter()
     print(f"Score: {score}")
+    print(f"Time taken: {format_time(t2-t1)}")
 
     if platform.system() == "Windows":
         subprocess.run("clip", text=True, input=str(score))
